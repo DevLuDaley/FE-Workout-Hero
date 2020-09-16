c2b16a2ea7386249ef90d63edc70c5ae12260d35
Lu Daley <46202132+DevLuDaley@users.noreply.github.com>
2020-09-16 11:53:41 -0400

    create an index.js file to call app

---
+++ src/index.js
@@ -0,0 +1,6 @@
+const app = new App()
+
+// const BACKEND_URL = 'localhost:3000';
+// fetch(`${BACKEND_URL}/test`)
+//   .then(response => response.json())
+//   .then(parsedResponse => console.log(parsedResponse));
