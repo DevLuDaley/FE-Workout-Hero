b7b1846e0a5135ead5c2efc262fa4bc1dff6bfd5
Lu Daley <46202132+DevLuDaley@users.noreply.github.com>
2020-09-29 19:42:14 -0400

    clean up handleAdd handleEdit & addWorkout methods

--- src/components/routines.js
+++ src/components/routines.js
@@ -40,54 +40,31 @@ class Routines {
     })
     })
         .then( this.assignRoutines.bind(this) )
-      // .then( this.dropdownSetup.bind(this) )
-      // .then(this.toggleEditForm.bind(this))
-      // .then( this.render.bind(this) )
       .catch( (error) => console.log(error) )
     //   .catch( (error) => console.log("you broke it son!") )
   }
 
 
   assignRoutines(){
-// this.getUniqueWorkouts()
-let routinesString = this.routines.map(routine => routine.renderRoutinesAndWorkouts()
-).join('')
-this.routineCard.innerHTML = routinesString;
-  // routineCard.append(roos)
-  // ! show list of routine names
-  // roo.innerHTML = routineHeadingsList
-  // ! show list of workout names
-  // list.innerHTML = routineWorkoutsList
-// .setAttribute("id", box.id))
-// var str = '<ul>'
-
-//  ! main page should only show list of routines at top with list of workouts in a separate section. There will be two buttons. btn1 = a show/edit screen (drag and drop should go here) while btn2 = create routine screen --. user should see a list of routines and a list of workoutsouts. The list of routines should have create edit and a delete buttons. creating a workout should just be a name... then user can select the workout from the list and use the edit screen to drag/drop workouts...(workout already exists, are you sure you want to add?)
-
-// this.routines.map(r =>
-// ! for each [routine], create a [new row] && set the new row's [id] to the [routine id].
-
-// this.routines.forEach(
-//       r => this.routinesTable.insertRow().classList.add('routines-list')
-//       // setAttribute("id", `${r.id}`)
-//       // setAttribute("id", `routine-${r.id}`)
-//       )
-// numbers.forEach((num) => {
-  // console.log(num)
-
+  let routinesString = this.routines.map(
+    routine => routine.renderRoutinesAndWorkouts()
+  ).join('')
+  this.routineCard.innerHTML = routinesString;
   }
 
   handleAddRoutine(event) {
     event.preventDefault()
-
     const routineParams =
     {
       "routine_name": this.routineName.value
     }
 
     this.adapter.createRoutine(routineParams)
-    .then( (routineJSON) => this.routines.push(new Routine(routineJSON)) )
-    .then( this.assignRoutines.bind(this) )
-    // .then( this.render.bind(this) )
+    .then(
+      (routineJSON) => 
+        this.routines.push(new Routine(routineJSON)) )
+    .then(
+      this.assignRoutines.bind(this) )
     .then(
       this.routinesForm.reset() 
     )
@@ -95,32 +72,10 @@ this.routineCard.innerHTML = routinesString;
 
     handleEdit(event) {
       event.preventDefault()
-      
-      // console.log('1 event.target.id',event.target.id)
-      
       let targetId = event.target.id.replace("edit-routine-", "")
-      
-      // console.log('2 target.id', targetId)
 
-      // console.log('3 Routines -> handleEdit -> event.target.id.includes(`edit-routine-${targetId}`', event.target.id.includes(`edit-routine-${targetId}`))
-
-      // console.log('4 -> `edit-routine-${event.target.id}`', `edit-routine-${event.target.id}`);
       if(event.target != null && event.target.id.includes(`edit-routine-${targetId}`))
-      
-      // console.log('5 event.target ', event.target );
-      
-      
-      
-      
       {
-          // console.log("5 LU handleEDITis TRUE")
-          // this.editRoutineName = document.getElementById('edit-routine-name')
-          // this.editRoutineWorkoutName = document.getElementById('edit-routine-workout-name')
-          // console.log('Routines -> handleEdit -> this.editRoutineWorkoutName', this.editRoutineWorkoutName);
-          
-          // console.log('6 Routines -> handleEdit -> input_name value', inputName.value);
-          // this.routineFormId = document.getElementById(`edit-routine-${event}`)
-          // console.log('Routines -> handleEdit -> this.routineFormId', this.routineFormId);
           let id = targetId
           let inputName = document.getElementById(`input-routine-${id}-workout-name`)  
 
@@ -136,95 +91,39 @@ this.routineCard.innerHTML = routinesString;
             ]
        }
               this.adapter.editRoutine(updateRoutineParams)
-              // .then( this.assignRoutines.bind(this) )
               .then( resp => this.addWorkout(resp))
       }
-      
-      // else {console.log("NOPE")}
-    // .then( this.assignRoutines.bind(this) )
-    // .then( this.render.bind(this) )
-    // .then(
-    //   this.routinesForm.reset() )
+
   }
 
   addWorkout(addWorkoutResponse){
     const { workouts } = addWorkoutResponse
-    // const { id, workout_name} = workouts
-// console.log(`addWorkoutResponse["workouts"]`,addWorkoutResponse["workouts"])
 
-// let apiWorkouts = workouts.map(function({ id,workout_name}){
-//   return id, workout_name}
-  // )
-  
-  // console.log(apiWorkouts)
-
-    let changedElement = document.getElementById(`routine-${addWorkoutResponse["id"]}-workouts`)
+    let editForm = document.getElementById(`edit-routine-${addWorkoutResponse["id"]}`)
+    
+    let routinesElement = document.getElementById(`routine-${addWorkoutResponse["id"]}-workouts`)
 
     console.log('Routines -> addWorkout -> addWorkoutResponse["id"]', addWorkoutResponse["id"]);
 
-    // console.log('Routines -> addWorkout -> changedElement', changedElement.innerHTML);
-    // this.routines = this.routines.find(id)
-    
-    // console.log('Routines -> addWorkout -> id', id);
-        let newBlock = addWorkoutResponse["workouts"].map(workout =>
+        let updatedRoutinesElement = addWorkoutResponse["workouts"].map(workout =>
           `
-          <section class="routine-workout-block"
-          id='workout-${workout.id}'>
-          <div class='routine-workout-names'>
-          ${workout.workout_name}
-          <section class='routine-workout-details'>
-          </section>
-          </div>
+          <section 
+            class="routine-workout-block"
+              id='workout-${workout.id}'>
+
+                <div class='routine-workout-names'>
+                  ${workout.workout_name}
+                  <section 
+                    class='routine-workout-details'>
+                  </section>
+                </div>
           </section>
           `
           ).join('')
 
-          changedElement.innerHTML = newBlock
-
-console.log('Routines -> addWorkout -> changedElement.innerHTML', changedElement.innerHTML);
-let editForm = document.getElementById(`edit-routine-${addWorkoutResponse["id"]}`)
-    // this.assignRoutines()
+    routinesElement.innerHTML = updatedRoutinesElement
   editForm.reset()
   }
-
-/**
- 
-"id": 2,
-  "routine_name": "Cardio Rush",
-  "workouts": [
-    {
-      "id": 14,
-      "workout_name": "volley ball",
-      "workout_type": "Cardio",
-      "distance": 2,
-      "duration": 4,
-      "created_at": "2020-09-26T18:39:27.523Z"
-    },
-    {
-      "id": 15,
-      "workout_name": "cricket",
-      "workout_type": "Cardio",
-      "distance": 8,
-      "duration": 10,
-      "created_at": "2020-09-26T18:39:27.529Z"
-    },
-    {
-      "id": 21,
-      "workout_name": "Break Dancing",
-      "workout_type": "Cardio",
-      "distance": 4,
-      "duration": 4,
-      "created_at": "2020-09-26T18:48:01.689Z"
-    }
-  ]
-}
-
-
-
- */
-
-
-
 
   handleDeleteRoutine(event) {
     if (event.target.dataset.action != null && event.target.dataset.action.includes("delete-routine")
